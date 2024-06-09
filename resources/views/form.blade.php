dd7cfffa64eab80ec58bf160e817147874d94a76
KepoDes <86428587+Kepoo311@users.noreply.github.com>
2024-06-09 21:52:34 +0700

    typo fix

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -59,7 +59,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="jabatan" class="block mb-2 text-sm font-medium text-gray-900">Jabatan</label>
-                        <input type="text" id="jabatan" name="jabatan" placeholder="Nomor telpon anda.." autocomplete="off"
+                        <input type="text" id="jabatan" name="jabatan" placeholder="Jabatan anda..." autocomplete="off"
                             value="{{ old('jabatan') }}"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                         @error('jabatan')
